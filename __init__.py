631f26c78c6c3eee5b93b38768157f06c2442416
kool-23-herc <nilszinsou@gmail.com>
2025-02-14 16:11:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,65 +3,8 @@ from flask import render_template
 from flask import json
 import sqlite3
                                                                                                                                        
-# app = Flask(__name__)
-
-# # @app.route("/contact/")
-# # def MaPremiereAPI():
-# #     return "<h2>Ma page de contact</h2>"
-# @app.route("/contact/")
-# def MaPremiereAPI():
-#     return render_template("contact.html") 
-
-# @app.route('/calcul_carre/<int:val_user>')
-# def carre(val_user):
-#     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user*val_user)
-  
-# @app.route('/somme/<int:val1>/<int:val2>')
-# def somme(val_user):
-#     return "<h2>La somme de nos valeurs est : </h2>" + str(val1+val2)
-
-
+app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-# @app.route('/')
-# def hello_world():
-#     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>" #comm
-
-# @app.route('/exercices/')
-# def exercices():
-#     return render_template('exercices.html')
-  
-# @app.route('/pi/<int:val1>')
-# def pi(val1):
-#   if val1%2==0:
-#     return "<h2>pair</h2>"
-#   else:
-#     return "<h2>impaire</h2>"   
-
-
-# if __name__ == "__main__":
-#   app.run(debug=True)
-
-
-
-
-
-
-
-
-app = Flask(__name__)
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("contact.html") 
-
-@app.route('/calcul_carre/<int:val_user>')
-def carre(val_user):
-    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user*val_user)
-  
-@app.route('/somme/<int:val1>/<int:val2>')
-def somme(val1, val2):
-    return "<h2>La somme de nos valeurs est : </h2>" + str(val1 + val2)
-
 @app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
@@ -69,24 +12,35 @@ def hello_world():
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
-  
-@app.route('/pi/<int:val1>')
-def pi(val1):
-    if val1 % 2 == 0:
-        return "<h2>pair</h2>"
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template('contact.html')
+
+@app.route('/calcul_carre/<int:val>')
+def carre(val):
+    return "<h2>Le carré de la valeur est : </h2>" + str(val*val)
+
+@app.route('/somme/<int:val1>/<int:val2>')
+def somme(val1,val2):
+    return "<h2>La somme de vos valeurs est : </h2>" + str(val1+val2)
+
+@app.route('/parite/<int:val>')
+def parite(val):
+    if val%2==0:
+        return "La valeur que vous avez donné est paire"
     else:
-        return "<h2>impaire</h2>"
+        return "La valeur que vous avez donné est impaire"
 
-@app.route('/somme/')
-def afficher_formulaire_somme():
-    return render_template('somme.html')
+@app.route('/sommetotale/<path:val>')
+def sommetot(val):
+    sommet = list(map(int, val.split('/')))
+    sommetotale = sum(sommet)
+    return "La somme totale des valeurs est :" + str(sommetotale)
 
-@app.route('/calculer_somme', methods=['POST'])
-def calculer_somme():
-    valeurs = request.form['valeurs']
-    valeurs_liste = [int(v) for v in valeurs.split(',')]
-    somme_valeurs = sum(valeurs_liste)
-    return f"<h2>La somme des valeurs est : {somme_valeurs}</h2>"
+@app.route('/cnam/')
+def cnampage():
+    return render_template('cnam.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
