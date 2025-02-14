a5fed823b2354a58b791abc02e1fb2fa98cdf903
kool-23-herc <nilszinsou@gmail.com>
2025-02-14 11:58:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,9 +17,9 @@ def hello_world():
 def exercices():
     return render_template('exercices.html')
   
-# @app.route("/contact/")
-# def MaPremiereAPI():
-#     return render_template("contact.html")
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("contact.html")
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
